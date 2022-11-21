48ab204bb8a3a4969c8644b119d91dcadcee08e4
KKRISHNA SAXENA <saxena.kkrishna@gmail.com>
2022-11-21 23:43:41 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const mongoURI= "mongodb+srv://iecse:iuSXIJFIUedUaLSs@cluster0.lx0lp9y.mongodb.net/gbm22";
+const mongoURI= "mongodb+srv://kkrishna007:iecseftw@cluster1.aumb3gl.mongodb.net/iecse";
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI,()=>{console.log("Connected to Mongo Successfully");
